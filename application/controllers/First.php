b371af3aafda0a325a2e4f4d152b5b9e0963d882
Duy Le <duyt.le@hotmail.ca>
2015-09-24 14:52:44 -0700

    Fixed first link

---
+++ application/controllers/First.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ * 
+ * controllers/Welcome.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class First extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->first();
+		/*
+        $this->data['who'] = $source['who'];
+		$this->data['mug'] = $source['mug'];
+		$this->data['what'] = $source['what'];
+		*/
+		$this->data = array_merge($this->data, $source);
+
+        $this->render();
+    }
+
+}
+
+/* End of file Welcome.php */
+/* Location: application/controllers/Welcome.php */
